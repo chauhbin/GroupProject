5e78b987719f158f2b56130f22532d0c61d3fac6
chauhbin <chauhbin@sheridancollege.ca>
2025-04-07 20:15:26 -0400

    last

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -8,7 +8,7 @@ package ca.sheridancollege.project;
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  * @author Navdeep Kaur
- * @author Binderpal Singh Chauhan
+ * @author Binderpal Singh 
  */
 
 
